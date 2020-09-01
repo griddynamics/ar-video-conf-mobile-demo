0652ca8894ca207f7b1a17a9c087971a3590a85d
imamontov <ivan.mamontov@gmail.com>
2020-09-01 17:26:48 +0300

    Latest model 32x32 with 8 filters

--- python/unet.py
+++ python/unet.py
@@ -14,13 +14,13 @@ from keras_unet.utils import plot_imgs
 from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
-model_filename = 'segm_model_v6.h5'
-checkpoint_path = "training_6/cp-{epoch:04d}.ckpt"
+model_filename = 'segm_model_v17.h5'
+checkpoint_path = "training_17/weights.{epoch:02d}-{val_loss:.2f}.hdf5"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 if not os.path.exists(checkpoint_dir):
     os.mkdir(checkpoint_dir)
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 # From [TensorFlow guide][1]https://www.tensorflow.org/guide/gpu#limiting_gpu_memory_growth
 gpus = tf.config.list_physical_devices('GPU')
 if gpus:
@@ -32,15 +32,15 @@ if gpus:
     except RuntimeError as e:
         print(e)
 
-shape = (256, 256)
+shape = (32, 32)
 
 # model = vanilla_unet(input_shape=(196, 196, 3))
 
 model = custom_unet(
-    (256, 256, 3),
-    use_batch_norm=False,
+    (32, 32, 3),
+    use_batch_norm=True,
     num_classes=1,
-    filters=16,
+    filters=8,
     use_attention=True,
     dropout=0.2,
     output_activation='sigmoid'
@@ -54,12 +54,14 @@ model.compile(
     metrics=[iou, iou_thresholded]
 )
 
+print(model.summary())
+
 if os.path.exists(model_filename):
     print("Loading model from checkpoint {}".format(model_filename))
     model.load_weights(model_filename)
 
 while True:
-    masks = random.sample(glob.glob(os.path.join("faces", "masks", "*.png")), 5000)
+    masks = glob.glob(os.path.join("faces", "masks", "*.png"))
     orgs = list(map(lambda x: x.replace(".png", ".jpg").replace("masks", "images"), masks))
 
     imgs_list = []
@@ -76,11 +78,16 @@ while True:
     x = np.asarray(imgs_np, dtype=np.float32) / 255
     y = np.asarray(masks_np, dtype=np.float32) / 255
     print(x.max(), y.max())
+    del imgs_np
+    del masks_np
 
     y = np.reshape(y, (y.shape[0], y.shape[1], y.shape[2], 1))
     print(x.shape, y.shape)
 
     x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.30, random_state=0)
+
+    del x
+    del y
 
     print("x_train: ", x_train.shape)
     print("y_train: ", y_train.shape)
@@ -88,7 +95,7 @@ while True:
     print("y_val: ", y_val.shape)
 
     train_gen = get_augmented(
-        x_train, y_train, batch_size=128,
+        x_train, y_train, batch_size=1024,
         data_gen_args=dict(
             rotation_range=15.,
             width_shift_range=0.05,
@@ -109,8 +116,8 @@ while True:
 
     history = model.fit(
         train_gen,
-        steps_per_epoch=40,
-        epochs=150,
+        steps_per_epoch=35,
+        epochs=15150,
         validation_data=(x_val, y_val),
         callbacks=[callback_checkpoint]
     )
